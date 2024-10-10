7ebbc290d0cf5dc83dffd9cb1277d32b59ac3a70
gmykbalaji <gmykbalaji@gmail.com>
2024-10-10 13:45:57 +0530

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,11 @@
+﻿namespace Balanugettestlogger
+{
+    public class Logger
+    {
+        public void logmessage(string message)
+        {
+            Console.Write(message);
+        }
+
+    }
+}
